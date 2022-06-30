3ceb91ef05395da10bf89c8130b9fc6c5524736d
MOCH EKO BUDI SETIAWAN <gseqoo@gmail.com>
2022-06-30 10:29:32 +0700

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,35 @@
+ 
+//mode dark
+
+ var content = document.getElementsByTagName('body')[0];
+        var darkMode = document.getElementById('dark-change');
+        darkMode.addEventListener('click', function(){
+            darkMode.classList.toggle('active');
+            content.classList.toggle('night');
+        })
+
+
+//contac form mengalihkan sukses dan loader
+
+const contactForm = document.getElementById("contact-form");
+const loader = document.querySelector(".loader");
+
+loader.style.display = "none";
+
+contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  loader.style.display = "block";
+  const url = e.target.action;
+  const formData = new FormData(contactForm);
+
+  fetch(url, {
+    method: "POST",
+    body: formData,
+    mode: "no-cors",
+  })
+    .then(() => {
+      loader.style.display = "none";
+      window.location.href = "https://ekobudi.my.id/thankyou.html";
+    })
+    .catch((e) => alert("Error occured"));
+});
